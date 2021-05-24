ce54ddbab1a1652f56aea2add4e2d070751af919
Sanjay Gonsalves <snjgonsalves@gmail.com>
2021-05-23 20:02:44 -0700

    Home page created

---
+++ portfolio2021/src/components/Social.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+class Social extends Component {
+render() {
+return (
+<div className="social">
+ 
+<a href="https://github.com" ><i class="fab fa-github"></i></a>
+<a href="https://Instagram.com" ><i class="fab fa-instagram"></i></a>
+<a href="https://www.facebook.com" ><i class="fab fa-facebook-f"></i></a>
+<a href="https://Linkedin.com" ><i class="fab fa-linkedin-in"></i></a>
+
+
+</div>
+)
+}
+}
+export default Social
