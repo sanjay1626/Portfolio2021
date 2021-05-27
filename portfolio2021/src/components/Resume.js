21b19250ed16d38feae3b441d7a6461a14848249
Sanjay Gonsalves <snjgonsalves@gmail.com>
2021-05-26 18:57:05 -0700

    Fixed font and alignment

--- portfolio2021/src/components/Resume.js
+++ portfolio2021/src/components/Resume.js
@@ -6,36 +6,34 @@ class Resume extends Component{
         return (
             <section>
              <div className ="row" id="resume">
-                
-                    
                     <h1>
                         <span>Education</span></h1>
-                   
                         <h3>
-                    <span>UCSD Extension</span><br />
-                        <span>Web Development BootCamp</span><br />
-                        <span> March 2021</span><br /><br />
+                    <div className="info">
+                        <h2>University of California San Diego Extension</h2>
+                        <span>Boot Camp Web Developement</span><br />
+                        <div className="date">March 2021</div>
+                    </div>
+                   
+                    <div className="info">
+                        <h2>University of Baguio</h2>
+                        <span>Bachelor's in Science of Computer Engineering</span><br />
+                        <div className="date">May 2004</div>
+                    </div><br />
 
-
-                        <span>University of Baguio</span><br />
-                        <span>BS Computer Engineering</span><br />
-                        <span>May 2004</span><br /><br />
                     </h3>
 
-                 
-
-                
                 <div className ="row work" >
                         <h1><span>Work</span></h1>
-                        <h3>
-                            <span>FC Associate 2020 - present</span><br />
+                        <h3 className="info">
+                            <span>Fullfillment Center Associate 2020 - present</span><br />
                             <span>Amazon Prime LLC</span> <br />
                             <br />
-                            <span>EC Supervisor</span><br />
+                            <span>Express Center Supervisor</span><br />
                             <span>BlackArrow Express 2016 - 2019</span> <br />
                             <br />
                             <span>Data Entry/ Area Supervisor</span><br />
-                            <span>Filmetrucs Inc. 2011 - 2011</span><br />
+                            <span>Filmetrics Inc. 2011 - 2011</span><br />
                             <br />
                             <span>Customer Service Representative</span><br />
                             <span>People Support 2008 - 2010</span><br />
@@ -46,15 +44,14 @@ class Resume extends Component{
 
                         </h3>
                 </div>
-                <div className ="row">
+                <div className ="row " id ="skill">
                     <h1><span>Skills</span></h1>
-                <h3>
-                    <span>HTML</span><br />
+                <h3 className="info">
+                    <span>HTML/CSS</span><br />
                     <br />
                     <ProgressBar completed = {85} width ={500}/><br />
-                    <br />
-
-                    <span>CSS</span><br />
+                    
+                    <span>JavaScript</span><br />
                     <br />
                     <ProgressBar completed = {85} width ={500} bgColor= '#6e7881' /><br />
                     <br />
