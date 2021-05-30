2e8923f60d815849649e3a1097a03897ba077e14
Sanjay Gonsalves <snjgonsalves@gmail.com>
2021-05-29 20:41:58 -0700

    adjusted footer size and added social links

--- portfolio2021/src/components/Footer.js
+++ portfolio2021/src/components/Footer.js
@@ -7,9 +7,14 @@ class Footer extends Component{
             <div className="row" >
                 <div className = "col">
                     <ul className ="social-links">
-                       
+                        <li> <a href="https://github.com/sanjay1626" ><i class="fab fa-github"></i> </a></li>
+                        <li><a href="https://Instagram.com" ><i class="fab fa-instagram"></i> </a></li>
+                        <li><a href="https://www.facebook.com/sanjay.gonsalves.96" ><i class="fab fa-facebook-f"></i> </a></li>
+                        <li><a href="www.linkedin.com/in/sanjay-gonsalves-61a74b39" ><i class="fab fa-linkedin-in"></i> </a></li>
+                    
                     
                     <div className ='copyright'>
+                           
                         <li>&copy; Copyright 2021 Sanjay Gonsalves</li>
                         <li>Design by <a title="Styleshout" href="https://github.com/sanjay1626">Monkey@Brain</a></li>
                     </div>  
