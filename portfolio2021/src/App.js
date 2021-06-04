6df0108ae3068edef3a7265ce97708700c299231
Sanjay Gonsalves <snjgonsalves@gmail.com>
2021-06-04 15:49:59 -0700

    fixing

--- portfolio2021/src/App.js
+++ portfolio2021/src/App.js
@@ -11,7 +11,7 @@ import Footer from './components/Footer';
 function App() {
   return (
    
-          <div className ="header">
+          <div className ="App">
             
               
                 <Home />
